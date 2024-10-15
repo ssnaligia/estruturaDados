2e5c4b82b51bf7056acdec04ac7e3ce764b27a5f
ssnaligia <ana.ligia@aluno.ifsp.edu.br>
2024-10-14 22:08:59 -0300

    exercício busca

---
+++ 2. outubro - 2024/exercicioBusca/index.c
@@ -0,0 +1,17 @@
+#include "utilitario.h"
+
+void load_file_classified(int array[], int size){
+    srand( (unsigned) time(NULL) );
+
+    array[0] = rand() % 1000000;
+    for(int i = 1; i != size; i++){
+        array[i] = array[i-1] + ( rand() % 100000 );
+    }
+}
+
+void print_file(int array[], int size){
+    for(int i = 0; i != size; i += 1){
+        printf("%d ", array[i]);
+    }
+    printf("\n\n");
+}
