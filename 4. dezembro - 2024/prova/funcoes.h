c3b1170a72b6677515c14c1152d3268facb8de37
ssnaligia <ana.ligia@aluno.ifsp.edu.br>
2024-12-06 23:23:22 -0300

    alt prova

--- 4. dezembro - 2024/prova/funcoes.h
+++ 4. dezembro - 2024/prova/funcoes.h
@@ -1,23 +1,22 @@
 #ifndef FUNCOES_H
-#define FUNCOES_H
+    #define FUNCOES_H
 
-typedef struct {
-    char bloco[2];
-    char identificacao[4];
-    int capacidadeAtual;
-    int capacidadeMaxima;
-} SalaTeorica;
+    typedef struct {
+        char bloco[2];
+        char identificacao[4];
+        int capacidadeAtual;
+        int capacidadeMaxima;
+    } SalaTeorica;
 
-typedef struct {
-    char bloco[2];
-    char identificacao[4];
-    int capacidadeAtual;
-    int capacidadeMaxima;
-} Laboratorio;
+    typedef struct {
+        char bloco[2];
+        char identificacao[4];
+        int capacidadeAtual;
+        int capacidadeMaxima;
+    } Laboratorio;
 
-double calcularTaxaOcupacao(SalaTeorica *salas, int quantidadeSalas);
-int calcularComputadoresNecessarios(Laboratorio *labs, int quantidadeLabs);
-void exibirEspacosOrdenados(SalaTeorica *salas, int quantidadeSalas, Laboratorio *labs, int quantidadeLabs);
-void definirDadosIniciais(SalaTeorica *salas, Laboratorio *labs);
-
+    double calcularTaxaOcupacao(SalaTeorica *salas, int quantidadeSalas);
+    int calcularComputadoresNecessarios(Laboratorio *labs, int quantidadeLabs);
+    void exibirEspacosOrdenados(SalaTeorica *salas, int quantidadeSalas, Laboratorio *labs, int quantidadeLabs);
+    void definirDadosIniciais(SalaTeorica *salas, Laboratorio *labs);
 #endif
